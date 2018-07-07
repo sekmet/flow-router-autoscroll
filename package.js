4e8a6f8a2da2f539641df0f4f64d9d0d08b32216
Carlos Guimaraes <sekmet@open4g.com>
2018-07-07 16:10:54 -0300

    Update package.js
    Replace kadira:flow-router by ostrio:flow-router-extra

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
-  name: 'tomwasd:flow-router-autoscroll',
-  version: '0.0.2',
-  summary: 'Fixes page position after changing pages using Flow Router',
-  git: 'https://github.com/tomwasd/flow-router-autoscroll',
+  name: 'skt:flow-router-autoscroll',
+  version: '0.0.1',
+  summary: 'Fixes page position after changing pages using Flow Router Extra',
+  git: 'https://github.com/sekmet/flow-router-autoscroll',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.use('kadira:flow-router@2.0.0');
+  api.use('ostrio:flow-router-extra');
   api.addFiles('client/autoscroll.js', 'client');
   api.export('FlowRouterAutoscroll', 'client');
 });
